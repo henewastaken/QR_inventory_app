04e5ef2bf575fd499a1f09e806cd774054c7dfa6
henewastaken <hene.andersson@gmail.com>
2021-12-22 17:45:13 +0200

    implemented base qr scanner functionality

--- app/src/main/java/com/example/testi/NewItem.kt
+++ app/src/main/java/com/example/testi/NewItem.kt
@@ -1,13 +1,20 @@
 package com.example.testi
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.zxing.integration.android.IntentIntegrator
 
 class NewItem : AppCompatActivity() {
+
+    private lateinit var mQrResultLauncher : ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_item)
@@ -15,6 +22,19 @@ class NewItem : AppCompatActivity() {
         // Set custom toolbar as app bar
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        // Alternative to "onActivityResult", because that is "deprecated"
+        mQrResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if(it.resultCode == Activity.RESULT_OK) {
+                val result = IntentIntegrator.parseActivityResult(it.resultCode, it.data)
+
+                if(result.contents != null) {
+                    // Do something with the contents (this is usually a URL)
+                    Toast.makeText(this, result.contents, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        startScanner()
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_bar, menu)
@@ -31,5 +51,16 @@ class NewItem : AppCompatActivity() {
             R.id.menu_new_item -> Toast.makeText(this, "new item", Toast.LENGTH_SHORT).show()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    // Start the QR Scanner
+    private fun startScanner() {
+        val scanner = IntentIntegrator(this)
+        // QR Code Format
+        scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        // Set Text Prompt at Bottom of QR code Scanner Activity
+        scanner.setPrompt("QR Code Scanner Prompt Text")
+        // Start Scanner (don't use initiateScan() unless if you want to use OnActivityResult)
+        mQrResultLauncher.launch(scanner.createScanIntent())
     }
 }
